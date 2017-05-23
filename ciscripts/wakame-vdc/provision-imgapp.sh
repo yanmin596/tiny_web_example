ba1d15fe4e5833db5ed6bcdaad0c1698803da764
t-iwano <t-iwano@axsh.net>
2017-05-23 20:05:05 +0900

    Fixed ruby version

--- ciscripts/wakame-vdc/provision-imgapp.sh
+++ ciscripts/wakame-vdc/provision-imgapp.sh
@@ -39,13 +39,13 @@ echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 exec $SHELL -l
 
 # install build require for ruby-build
-yum install -y gcc openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
+yum install -y gcc openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel libffi-devel
 
 # install ruby-build
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # install ruby
-ruby_version="2.0.0-p598"
+ruby_version="2.2.2"
 
 rbenv install -v "${ruby_version}"
 rbenv rehash
